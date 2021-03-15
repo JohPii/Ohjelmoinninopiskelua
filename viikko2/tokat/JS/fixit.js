71a932d77e31b3da58654f755d3bc54f072865f0
JohPii <johanna.piiroinen@edu.keuda.fi>
2021-03-15 15:36:33 +0200

    muutoksia sijainteihin

---
+++ viikko2/tokat/JS/fixit.js
@@ -0,0 +1,14 @@
+
+function posNeg() {
+
+luku1 = document.getElementById ("luku1").value;
+
+if ( luku1 < 0 ) {
+  alert("Kirjoittamasi luku " + luku1 + " on negatiivinen.");
+  console.log("Negatiivinen luku!");
+}
+else {
+  alert("Kirjoittamasi luku " + luku1 + " on positiivinen");
+  console.log("Positiivinen luku!");
+}
+}
